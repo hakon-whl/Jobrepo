fdf1a47df91e6e63b108fe6ab19ed08578a23c79
wahlh <hakon.wahl@gmail.com>
2025-06-27 18:45:22 +0200

    done

--- projekt/backend/scrapers/__init__.py
+++ projekt/backend/scrapers/__init__.py
@@ -1,21 +1,8 @@
-"""
-projekt.backend.scrapers
-
-Enthält alle Web-Scraper für die relevanten Job-Plattformen.
-"""
-
-__version__ = "0.1.0"
+# projekt/backend/scrapers/__init__.py
+from .xing_scraper import XingScraper
+from .stepstone_scraper import StepStoneScraper
 
 __all__ = [
-    "RequestBaseScraper",
-    "SeleniumBaseScraper",
-    "StepstoneScraper",
-    "XingScraper",
-    "StellenanzeigenScraper",
+  "StepStoneScraper",
+  "XingScraper"
 ]
-
-from .request_base_scraper    import RequestBaseScraper
-from .selenium_base_scraper   import SeleniumBaseScraper
-from .stepstone_scraper       import StepstoneScraper
-from .xing_scraper            import XingScraper
-from .stellenanzeigen_scraper import StellenanzeigenScraper
