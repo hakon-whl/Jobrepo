fdc369d139ec9f3f2379201b64635b6710b3f6f7
wahlh <hakon.wahl@gmail.com>
2025-06-27 18:46:01 +0200

    done

--- projekt/backend/utils/__init__.py
+++ projekt/backend/utils/__init__.py
@@ -1,16 +1,8 @@
-"""
-projekt.backend.utils
-
-Hilfsfunktionen für HTML-Parsing und PDF-Erstellung.
-"""
-
 __version__ = "0.1.0"
 
 __all__ = [
     "extract_text_from_selector",
     "extract_attribute_from_selector",
-    "PdfUtils",
 ]
 
 from .html_parser import extract_text_from_selector, extract_attribute_from_selector
-from .pdf_utils import PdfUtils
